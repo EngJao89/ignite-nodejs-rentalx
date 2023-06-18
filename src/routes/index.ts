57e2c6f75f28608fd28c778c48657dc5dad3e733
EngJao89 <eng_joao@outlook.com>
2023-06-17 21:08:58 -0300

    feat(rx39-user-routes): implementa rota de usuários

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,21 @@
-import { Router } from 'express';
-import { categoriesRoutes } from './categories.routes';
-import { specificationRoutes } from './specification.routes';
+import { Router } from "express";
+
+import { authenticateRoutes } from "./authenticate.routes";
+import { carsRoutes } from "./cars.routes";
+import { categoriesRoutes } from "./categories.routes";
+import { passwordRoutes } from "./password.routes";
+import { rentalRoutes } from "./rental.routes";
+import { specificationsRoutes } from "./specifications.routes";
+import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
 router.use("/categories", categoriesRoutes);
-router.use("/specifications", specificationRoutes);
+router.use("/specifications", specificationsRoutes);
+router.use("/users", usersRoutes);
+router.use("/cars", carsRoutes);
+router.use("/rentals", rentalRoutes);
+router.use("/password", passwordRoutes);
+router.use(authenticateRoutes);
 
-export { router }
+export { router };
