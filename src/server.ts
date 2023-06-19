3fc62ecff2638ca9171aa8b9ad708580fac0d6f0
EngJao89 <eng_joao@outlook.com>
2023-06-18 22:30:24 -0300

    feat(rx44-exceptions): implementa tratamento de exceção para o servidor

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
+
+import { AppError } from "@shared/errors/AppError";
 
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
@@ -13,5 +15,19 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 app.listen(3333, () => console.log("Sever is running!"));
